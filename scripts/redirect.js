5899d506f00f12dfd9c32c60390c04636ce8642e
Selwyn Leeke <selwyn.leeke@verint.com>
2025-11-06 16:15:03 -0800

    Minor changes based on review

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,5 +1,5 @@
 (function() {
-  var path = window.location.pathname.replace('/mobile/android/latest/', '/mobile/android/7.5.1/');
+  var path = window.location.pathname.replace('/mobile/android/latest/', '/mobile/android/7.5.2/');
   if (window.location.pathname.startsWith('/public-packages/mobile/android/latest/')) {
     window.location.href = path;
   }
