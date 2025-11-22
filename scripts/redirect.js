0b44a5e8cc1953bdfa7566eb159fcbbc7ff6de36
Selwyn Leeke <selwyn.leeke@verint.com>
2025-11-21 16:03:22 -0800

    VOC-60335: Update docs for version 7.6.0

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,5 +1,5 @@
 (function() {
-  var path = window.location.pathname.replace('/mobile/android/latest/', '/mobile/android/7.5.2/');
+  var path = window.location.pathname.replace('/mobile/android/latest/', '/mobile/android/7.6.0/');
   if (window.location.pathname.startsWith('/public-packages/mobile/android/latest/')) {
     window.location.href = path;
   }
