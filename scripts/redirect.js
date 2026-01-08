9fc5310fba51a5b616cff411df1dc5f2f24edba1
Shang Wu <shang.wu@verint.com>
2026-01-08 14:11:06 -0800

    Update docs for version 7.6.1

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,5 +1,5 @@
 (function() {
-  var path = window.location.pathname.replace('/mobile/android/latest/', '/mobile/android/7.6.0/');
+  var path = window.location.pathname.replace('/mobile/android/latest/', '/mobile/android/7.6.1/');
   if (window.location.pathname.startsWith('/public-packages/mobile/android/latest/')) {
     window.location.href = path;
   }
